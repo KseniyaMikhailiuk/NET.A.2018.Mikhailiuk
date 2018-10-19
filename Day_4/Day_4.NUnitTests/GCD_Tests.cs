d9c85ea854c468ec6eee737615cd33e0ee4de8c1
Mikhailiuk Kseniya <mikhailiuk.k@gmail.com>
2018-10-19 21:38:44 +0300

    add day_4 (Find GCD)

---
+++ Day_4/Day_4.NUnitTests/GCD_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Day_4_GreatestCommonDivisor;
+using NUnit.Framework;
+
+namespace Day_4.NUnitTests
+{
+    [TestFixture]
+    public class GCD_Tests
+    {
+        [TestCase(9, -585, 81, 189)]
+        [TestCase(6, 78, 294, 570, 36)]
+        [TestCase(1, 661, 113)]
+        [TestCase(45, 45)]
+        public static void GetGcd_Test(int expected, params int[] test)
+        {
+            Assert.AreEqual(expected, GCD.GetGcd(out long runTime, test));
+        }
+
+        [TestCase(9, -585, 81, 189)]
+        [TestCase(6, 78, 294, 570, 36)]
+        [TestCase(1, 661, 113)]
+        [TestCase(45, 45)]
+        public static void GetGcdBinary_Test(int expected, params int[] test)
+        {
+            Assert.AreEqual(expected, GCD.GetGcdBinary(out long runTime, test));
+        }
+
+        public static void GetGcdBinary_NullTest(int expected, params int[] test)
+        {
+            Assert.Throws<ArgumentException>(() => GCD.GetGcdBinary(out long runTime, new int[0]));
+        }
+
+        public static void GetGcd_NullTest(int expected, params int[] test)
+        {
+            Assert.Throws<ArgumentException>(() => GCD.GetGcd(out long runTime, new int[0]));
+        }
+    }
+}
---
+++ Day_4/Day_4_GreatestCommonDivisor/GCD.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day_4_GreatestCommonDivisor
+{
+    public class GCD
+    {
+        private delegate int GcdAlgorithm(int first, int second);
+
+        /// <summary>
+        /// Finds greatest common divisor using Stain's algorithm
+        /// </summary>
+        /// <param name="runTime"></param>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static int GetGcdBinary(out long runTime, params int[] numbers)
+        {
+            var result = ProcessArray(numbers, new GcdAlgorithm(GcdFortwo));
+            runTime = result.runTime;
+            return result.gcd;
+        }
+
+        /// <summary>
+        /// Finds greatest common divisor using Euclidean algorithm
+        /// </summary>
+        /// <param name="runTime"></param>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static int GetGcd(out long runTime, params int[] numbers)
+        {
+            var result = ProcessArray(numbers, new GcdAlgorithm(GcdBinaryFortwo));
+            runTime = result.runTime;
+            return result.gcd;
+        }
+
+        private static (int gcd, long runTime) ProcessArray(int[] numbers, GcdAlgorithm algorithmType)
+        {
+            if (numbers.Length < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+            long startTime = sw.ElapsedMilliseconds;
+            int gcd = Math.Abs(numbers[0]);
+            for (int i = 1; i <= numbers.Length - 1; i++)
+            {
+                gcd = algorithmType.Invoke(Math.Abs(numbers[i]), gcd);
+            }
+
+            return (gcd, sw.ElapsedMilliseconds - startTime);
+        }
+
+        private static int GcdFortwo(int first, int second)
+        {
+            while (first != second)
+            {
+                if (first > second)
+                {
+                    first -= second;
+                }
+                else
+                {
+                    second -= first;
+                }
+            }
+
+            return first;
+        }
+
+        private static int GcdBinaryFortwo(int first, int second)
+        {
+            int shift;
+            if (first == 0 || second == 0)
+            {
+                return first | second;
+            }
+
+            for (shift = 0;  ((first | second) & 1) == 0; ++shift)
+            {
+                first >>= 1;
+                second >>= 1;
+            }
+
+            while ((first & 1) == 0)
+            {
+                first >>= 1;
+            }
+
+            do
+            {
+                while ((second & 1) == 0)
+                {
+                    second >>= 1;
+                }
+
+                if (first > second)
+                {
+                    Swap(ref first, ref second);
+                }
+
+                second = second - first;
+            }
+            while (second != 0);
+
+            return first << shift;
+        }
+
+        private static void Swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+    }
+}
